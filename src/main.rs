f407bec8a6c920d7bab0a76627f3e4082609a717
Tooraj Taraz <tooraj.info@gmail.com>
2022-09-03 17:38:32 +0430

    cleaning and restructering main

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ mod buffer_initializer;
 mod kernels;
 mod shaders;
 
-const HEIGHT: u32 = 500;
-const WIDTH: u32 = 500;
-
 #[derive(Copy, Clone)]
 struct Vertex {
     points: [f32; 2],
@@ -27,21 +24,34 @@ fn main() {
     use glium::Surface;
     use glutin::dpi::LogicalSize;
 
+    let mut width: u32 = 500;
+    let mut height: u32 = 500;
+    let mut fps: f32 = 24.0;
+    let mut kernel = kernels::get_kernel(kernels::Kernel::RANDOM, None);
+    let mut single_pixel = [1.0f32 / width as f32, 1.0 / height as f32];
+    let mut color = [0.4f32, 0.0, 0.6, 1.0];
+    let mut skip = 4;
+
+    let fragment_src = shaders::fragment::get_fragment_shader(
+        shaders::fragment::Activation::INVERSEGAUSSIAN,
+        false,
+        None,
+    );
+
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_always_on_top(true)
+        .with_max_inner_size(LogicalSize::new(height, width))
+        .with_resizable(false);
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
     let points = vec![
         Vertex::new(-1.0, -1.0),
         Vertex::new(1.0, -1.0),
         Vertex::new(-1.0, 1.0),
         Vertex::new(1.0, 1.0),
     ];
-
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new()
-        .with_always_on_top(true)
-        .with_max_inner_size(LogicalSize::new(HEIGHT, WIDTH))
-        .with_resizable(false);
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-
     let vertex_buffer = glium::VertexBuffer::new(&display, &points).unwrap();
     let indices = glium::IndexBuffer::new(
         &display,
@@ -49,39 +59,49 @@ fn main() {
         &[0, 1, 2, 1, 2, 3u16],
     )
     .unwrap();
-
     let program = glium::Program::from_source(
         &display,
         shaders::vertex::VERTEX_SRC,
-        shaders::fragment::get_fragment_shader(
-            shaders::fragment::Activation::INVERSEGAUSSIAN,
-            false,
-            None,
-        )
-        .as_str(),
+        fragment_src.as_str(),
         None,
     )
     .unwrap();
 
-    let u_plane_base = buffer_initializer::new_as_texture(
+    let mut u_plane_base = buffer_initializer::new_as_texture(
         buffer_initializer::InitMode::RANDOM,
-        WIDTH,
-        HEIGHT,
+        width,
+        height,
         &display,
     );
-    let dest_texture = buffer_initializer::new_empty_texture(WIDTH, HEIGHT, &display);
+    let mut dest_texture = buffer_initializer::new_empty_texture(width, height, &display);
     dest_texture.as_surface().clear_color(0.0, 0.0, 0.0, 1.0);
 
-    let kernel = kernels::get_kernel(kernels::Kernel::RANDOM, None);
+    let mut is_first: bool = true;
+    let mut do_calc: bool = true;
 
-    let mut is_first: &bool = &true;
-    let mut do_calc: &bool = &true;
     event_loop.run(move |ev, _, control_flow| {
         match ev {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
+                }
+                glutin::event::WindowEvent::Resized(size) => {
+                    height = size.height;
+                    width = size.width;
+                    single_pixel = [1.0f32 / width as f32, 1.0 / height as f32];
+
+                    u_plane_base = buffer_initializer::new_as_texture(
+                        buffer_initializer::InitMode::RANDOM,
+                        width,
+                        height,
+                        &display,
+                    );
+                    dest_texture = buffer_initializer::new_empty_texture(width, height, &display);
+                    dest_texture.as_surface().clear_color(0.0, 0.0, 0.0, 1.0);
+
+                    is_first = true;
+                    do_calc = true;
                 }
                 _ => return,
             },
@@ -94,38 +114,46 @@ fn main() {
         }
         let mut target_fb =
             glium::framebuffer::SimpleFrameBuffer::new(&display, &dest_texture).unwrap();
-        if *is_first {
+        if is_first {
             let uniforms = uniform! {
                 u_kernel: kernel,
-                u_do_calc: *do_calc,
-                u_color_mask: [1.0f32, 0.0, 0.0, 1.0],
-                u_single_pixel: [1.0f32/WIDTH as f32, 1.0/HEIGHT as f32],
+                u_do_calc: do_calc,
+                u_color_mask: color,
+                u_single_pixel: single_pixel,
                 u_plane : &u_plane_base,
 
             };
-            target_fb
-                .draw(
-                    &vertex_buffer,
-                    &indices,
-                    &program,
-                    &uniforms,
-                    &Default::default(),
-                )
-                .unwrap();
-            is_first = &false;
-            do_calc = &false;
+
+            for _ in 0..skip {
+                target_fb
+                    .draw(
+                        &vertex_buffer,
+                        &indices,
+                        &program,
+                        &uniforms,
+                        &Default::default(),
+                    )
+                    .unwrap();
+
+                target_fb.fill(
+                    &dest_texture.as_surface(),
+                    glium::uniforms::MagnifySamplerFilter::Nearest,
+                );
+            }
+            is_first = false;
+            do_calc = false;
         } else {
             let uniforms = uniform! {
                 u_kernel: kernel,
-                u_do_calc: *do_calc,
-                u_color_mask: [0.4f32, 0.0, 0.6, 1.0],
-                u_single_pixel: [1.0f32/WIDTH as f32, 1.0/HEIGHT as f32],
+                u_do_calc: do_calc,
+                u_color_mask: color,
+                u_single_pixel: single_pixel,
                 u_plane : &dest_texture,
 
             };
 
-            if *do_calc {
-                for _ in 0..1 {
+            if do_calc {
+                for _ in 0..skip {
                     target_fb
                         .draw(
                             &vertex_buffer,
@@ -141,7 +169,7 @@ fn main() {
                         glium::uniforms::MagnifySamplerFilter::Nearest,
                     );
                 }
-                do_calc = &false;
+                do_calc = false;
             } else {
                 let mut target = display.draw();
                 target
@@ -155,12 +183,12 @@ fn main() {
                     .unwrap();
 
                 target.finish().unwrap();
-                do_calc = &true;
+                do_calc = true;
             }
         }
-        // u_plane_next = Option::Some(&dest_texture);
-        let next_frame_time =
-            std::time::Instant::now() + std::time::Duration::from_nanos(26_666_667);
+
+        let next_frame_time = std::time::Instant::now()
+            + std::time::Duration::from_nanos(((1.0 / fps) * 1_000_000f32) as u64);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
     });
 }
